1421c472f0a27583c1c9a7d347f46f605da764b7
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-02-17 14:42:17 +0800

    removed auto-scaling for dyanamodb

--- tf/modules/aws-dynamodb-counter/main.tf
+++ tf/modules/aws-dynamodb-counter/main.tf
@@ -24,50 +24,50 @@ resource "aws_dynamodb_table" "table" {
   }
 }
 
-resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
-  max_capacity       = var.autoscale_max_capacity
-  min_capacity       = var.autoscale_min_capacity
-  resource_id        = "table/${var.table_name}"
-  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
-  service_namespace  = "dynamodb"
-}
+# resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
+#   max_capacity       = var.autoscale_max_capacity
+#   min_capacity       = var.autoscale_min_capacity
+#   resource_id        = "table/${var.table_name}"
+#   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+#   service_namespace  = "dynamodb"
+# }
 
-resource "aws_appautoscaling_policy" "dynamodb_table_read_policy" {
-  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_read_target.resource_id}"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.dynamodb_table_read_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.dynamodb_table_read_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.dynamodb_table_read_target.service_namespace
+# resource "aws_appautoscaling_policy" "dynamodb_table_read_policy" {
+#   name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_read_target.resource_id}"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.dynamodb_table_read_target.resource_id
+#   scalable_dimension = aws_appautoscaling_target.dynamodb_table_read_target.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.dynamodb_table_read_target.service_namespace
 
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "DynamoDBReadCapacityUtilization"
-    }
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "DynamoDBReadCapacityUtilization"
+#     }
 
-    target_value = var.target_utilization
-  }
-}
+#     target_value = var.target_utilization
+#   }
+# }
 
-resource "aws_appautoscaling_target" "dynamodb_table_write_target" {
-  max_capacity       = var.autoscale_max_capacity
-  min_capacity       = var.autoscale_min_capacity
-  resource_id        = "table/${var.table_name}"
-  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
-  service_namespace  = "dynamodb"
-}
+# resource "aws_appautoscaling_target" "dynamodb_table_write_target" {
+#   max_capacity       = var.autoscale_max_capacity
+#   min_capacity       = var.autoscale_min_capacity
+#   resource_id        = "table/${var.table_name}"
+#   scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+#   service_namespace  = "dynamodb"
+# }
 
-resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
-  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_write_target.resource_id}"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.dynamodb_table_write_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.dynamodb_table_write_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.dynamodb_table_write_target.service_namespace
+# resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
+#   name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_write_target.resource_id}"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.dynamodb_table_write_target.resource_id
+#   scalable_dimension = aws_appautoscaling_target.dynamodb_table_write_target.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.dynamodb_table_write_target.service_namespace
 
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
-    }
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+#     }
 
-    target_value = var.target_utilization
-  }
-}
+#     target_value = var.target_utilization
+#   }
+# }
