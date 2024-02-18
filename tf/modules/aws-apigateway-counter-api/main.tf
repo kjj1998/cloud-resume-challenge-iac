48f948bb3f9de14c1389082e560c9e17e3580fba
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-02-18 10:57:02 +0800

    fixed apigateway api name error

--- tf/modules/aws-apigateway-counter-api/main.tf
+++ tf/modules/aws-apigateway-counter-api/main.tf
@@ -35,7 +35,7 @@ resource "aws_apigatewayv2_stage" "lambda" {
 }
 
 resource "aws_apigatewayv2_route" "route" {
-  api_id    = aws_apigatewayv2_api.gateway.id
+  api_id    = aws_apigatewayv2_api.lambda.id
   
   route_key = "POST /updateViewCountInDynamoDB"
   target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
@@ -46,7 +46,7 @@ data "aws_lambda_function" "existing" {
 }
 
 resource "aws_apigatewayv2_integration" "lamda_integration" {
-  api_id           = aws_apigatewayv2_api.gateway.id
+  api_id           = aws_apigatewayv2_api.lambda.id
   integration_type = "AWS_PROXY"
 
   connection_type           = "INTERNET"
