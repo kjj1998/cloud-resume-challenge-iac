538966b60de112bde403952ba7206eb9654b83af
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-02-18 10:51:41 +0800

    updated apigateway module to include lambda permission, stage, integration, cloudwatch group

--- tf/modules/aws-apigateway-counter-api/main.tf
+++ tf/modules/aws-apigateway-counter-api/main.tf
@@ -1,11 +1,44 @@
-resource "aws_apigatewayv2_api" "gateway" {
+resource "aws_apigatewayv2_api" "lambda" {
   name          = var.api_name
   protocol_type = var.api_protocol
 }
 
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 0
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+  
+  name = "default stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
 resource "aws_apigatewayv2_route" "route" {
   api_id    = aws_apigatewayv2_api.gateway.id
+  
   route_key = "POST /updateViewCountInDynamoDB"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
 data "aws_lambda_function" "existing" {
@@ -29,4 +62,13 @@ resource "aws_apigatewayv2_stage" "stage" {
   api_id = aws_apigatewayv2_api.gateway.id
   name   = "$default"
   auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = data.aws_lambda_function.existing.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
