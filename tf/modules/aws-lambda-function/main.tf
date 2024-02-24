dd54f4160b8fdb19ad106da3840b722d1a6a6cf4
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-02-24 17:11:41 +0800

    added role and policy for lambda

--- tf/modules/aws-lambda-function/main.tf
+++ tf/modules/aws-lambda-function/main.tf
@@ -17,15 +17,44 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+resource "aws_iam_role" "lambda_role_tf" {
+  name               = "iam_for_lambda_tf"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "lambda_policy_tf" {
+  name        = "lambda_policy_tf"
+  path        = "/"
+  description = "lambda policy for tf"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+            "dynamodb:DeleteItem",
+            "dynamodb:GetItem",
+            "dynamodb:PutItem",
+            "dynamodb:Scan",
+            "dynamodb:UpdateItem",
+            "dynamodb:DescribeTable"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:dynamodb:ap-southeast-1:271407076537:table/*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role_tf.name
+ policy_arn  = aws_iam_policy.lambda_policy_tf.arn
 }
 
 resource "aws_lambda_function" "update_view_count_lambda" {
   filename = "${path.cwd}/scripts/update_view_count.zip"
   function_name = var.function_name
-  role = aws_iam_role.iam_for_lambda.arn
+  role = aws_iam_role.lambda_role_tf.arn
   handler = "main.lambda_handler"
   runtime = "python3.12"
   source_code_hash = data.archive_file.update_view_count.output_base64sha256
